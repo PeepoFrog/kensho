b81696daeae12e9592bf2392ebd7bf829b858ea6
PeepoFrog <organizm999@gmail.com>
2024-06-02 17:00:43 +0300

    validator info saveCheck

--- types/endpoint/shidai/status.go
+++ types/endpoint/shidai/status.go
@@ -1,13 +1,29 @@
 package shidai
 
-type ComponentInfo struct {
+type componentInfo struct {
 	Version string `json:"version"`
 	Infra   bool   `json:"infra"`
 }
 
 type Status struct {
-	Sekai  ComponentInfo `json:"sekai"`
-	Interx ComponentInfo `json:"interx"`
-	Shidai ComponentInfo `json:"shidai"`
-	Syslog ComponentInfo `json:"syslog-ng"`
+	Sekai  componentInfo `json:"sekai"`
+	Interx componentInfo `json:"interx"`
+	Shidai componentInfo `json:"shidai"`
+	Syslog componentInfo `json:"syslog-ng"`
+}
+
+type ValidatorStatus string
+
+const (
+	Paused   ValidatorStatus = "PAUSED"
+	Active   ValidatorStatus = "ACTIVE"
+	Inactive ValidatorStatus = "INACTIVE"
+	Jailed   ValidatorStatus = "JAILED"
+)
+
+type Validator struct {
+	Validator        bool            `json:"validator"`
+	ClaimSeat        bool            `json:"claim_seat"`
+	ValidatorAddress string          `json:"validator_address"`
+	ValidatorStatus  ValidatorStatus `json:"validator_status "`
 }
